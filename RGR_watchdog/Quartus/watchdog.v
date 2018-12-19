06dd3234841a1c23bfeb95d1b1b2932054a2012c
maximwowpro <maxshvayuk@gmail.com>
2018-12-19 02:43:51 +0200

    add RGR watchdog

--- RGR_watchdog/Quartus/watchdog.v
+++ RGR_watchdog/Quartus/watchdog.v
@@ -1,17 +1,17 @@
-module watchdog_timer (i_clk, i_clrwdt, i_wait_period, i_rst_period, o_fail_safe, o_hardware_rst, counter_1, counter_2, counter_3);
+module watchdog_timer (i_clk, i_clrwdt, i_wait_period, i_rst_period, o_fail_safe, o_hardware_rst);
 	input i_clk;
-	input i_clrwdt;				// clear watchdog, the CPU should provide this signal in a predermined period
-	input [31:0] i_wait_period;		// how long WDT wait for system feedback
-	input [31:0] i_rst_period;		// determine the period of active logic level at reset pin
+	input i_clrwdt;
+	input [31:0] i_wait_period;	// how long WDT wait for system feedback
+	input [31:0] i_rst_period;	// determine the period of active logic level at reset pin
 	output reg o_fail_safe;
 	output reg o_hardware_rst;
 	
-	output reg [31:0] counter_1;		// fail safe condition
-	output reg [31:0] counter_2;		// hardware reset condition
-	output reg [31:0] counter_3;		// sets period of active logic level at reset pin
+	reg [31:0] counter_1;		// fail safe condition
+	reg [31:0] counter_2;		// hardware reset condition
+	reg [31:0] counter_3;		// sets period of active logic level at reset pin
 
-	
-	always @* begin				//asynchronous clear watchdog
+	/*
+	always @* begin				//asynchronous reset
 		if (i_clrwdt) begin
 			counter_1 <= i_wait_period;
 			counter_2 <= i_wait_period;
@@ -20,8 +20,15 @@ module watchdog_timer (i_clk, i_clrwdt, i_wait_period, i_rst_period, o_fail_safe
 			o_hardware_rst <= 0;
 		end	
 	end
-	
+	*/
 	always @(posedge i_clk) begin
+		if (i_clrwdt) begin
+			counter_1 <= i_wait_period;
+			counter_2 <= i_wait_period;
+			counter_3 <= i_rst_period;
+			o_fail_safe <= 0;
+			o_hardware_rst <= 0;
+		end	
 		if (counter_1 == 32'd0)		// begin fail save state
 			o_fail_safe <= 1;
 		else if (counter_1 != 32'd0)
